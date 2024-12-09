784e34897f10e96d84c631a486768536ef10f2fa
Matt Porritt <matt.porritt@moodle.com>
2024-12-09 16:52:42 +1100

    MDL-82977 AI: Provider instances
    Add a new admin setting that allows rendering of a template.
    This means templated content can be rendered as part of
    admin forms, giving more flexibility to forms and their actions.

---
+++ admin/classes/admin/admin_setting_template_render.php
@@ -0,0 +1,93 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace core_admin\admin;
+
+use admin_setting;
+use core_plugin_manager;
+use core_text;
+
+/**
+ * Render a template as part of other admin settings.
+ * Use for rendering additional html in settings.
+ *
+ * @package    core_admin
+ * @subpackage admin
+ * @copyright  2024 Matt Porritt <matt.porritt@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class admin_setting_template_render extends admin_setting {
+    /** @var string The name of the template to render */
+    protected string $templatename;
+
+    /** @var array|\stdClass The context to pass to the template */
+    protected array|\stdClass $context;
+
+    /**
+     * Constructor.
+     *
+     * @param string $name The name of the setting.
+     * @param string $templatename The name of the template to render.
+     * @param array|\stdClass $context The context to pass to the template.
+     */
+    public function __construct(
+        string $name,
+        string $templatename,
+        array|\stdClass $context
+    ) {
+        $this->nosave = true;
+        $this->templatename = $templatename;
+        $this->context = $context;
+
+        parent::__construct($name, $templatename, '', '');
+    }
+
+    /**
+     * Always returns true, does nothing
+     *
+     * @return true
+     */
+    public function get_setting(): bool {
+        return true;
+    }
+
+    /**
+     * Always returns true, does nothing
+     *
+     * @return true
+     */
+    public function get_defaultsetting(): bool {
+        return true;
+    }
+
+    /**
+     * Always returns '', does not write anything
+     *
+     * @return string Always returns ''
+     */
+    // phpcs:disable VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+    public function write_setting($data): string {
+        // Do not write any setting.
+        return '';
+    }
+
+    #[\Override]
+    public function output_html($data, $query = ''): string {
+        global $OUTPUT;
+
+        return $OUTPUT->render_from_template($this->templatename, $this->context);
+    }
+}
