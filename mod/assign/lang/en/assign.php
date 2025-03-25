dccf0b8b1f7e9a102bc5ae9ed0f9728d1894f40e
Safat <safat.shahin@moodle.com>
2025-03-26 00:46:32 +1100

    MDL-83518 mod_assign: Add missing sms string

--- mod/assign/lang/en/assign.php
+++ mod/assign/lang/en/assign.php
@@ -584,6 +584,7 @@ You can see the status of your assignment submission:
 $string['submissioncopiedhtml'] = '<p>You have made a copy of your previous assignment submission for \'<i>{$a->assignment}</i>\'.</p>
 <p>You can see the status of your <a href="{$a->url}">assignment submission</a>.</p>';
 $string['submissioncopiedsmall'] = 'You have copied your previous assignment submission for {$a->assignment}';
+$string['submissioncopiedsms'] = 'You made a copy of your previous submission for {$a->assignment}: {$a->url}';
 $string['submissiondrafts'] = 'Require students to click the submit button';
 $string['submissiondrafts_help'] = 'If enabled, students will have to click a submit button to declare their submission as final. This allows students to keep a draft version of the submission on the system. If this setting is changed from "No" to "Yes" after students have already submitted, those submissions will be regarded as final.';
 $string['submissioneditable'] = 'Student can edit this submission';
