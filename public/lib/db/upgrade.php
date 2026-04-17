eb9f3e242c4a3417d661c0f9e654b42693dfd5c3
Safat <safat.shahin@moodle.com>
2026-04-17 16:14:31 +1000

    MDL-88351 theme_classic: Remove classic theme from core

--- public/lib/db/upgrade.php
+++ public/lib/db/upgrade.php
@@ -1942,5 +1942,19 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2026061600.01);
     }
 
+    if ($oldversion < 2026072200.01) {
+        // Remove theme_classic if no longer present.
+        if (!file_exists($CFG->dirroot . '/theme/classic/version.php')) {
+            // Migrate settings and uninstall the plugin only if it is installed.
+            if (get_config('theme_classic', 'version') !== false) {
+                upgrade_migrate_classic_theme_to_boost();
+                uninstall_plugin('theme', 'classic');
+            }
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2026072200.01);
+    }
+
     return true;
 }
